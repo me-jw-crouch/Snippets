f0276caac5c20bcc2f534bf504bab4efe1398700
me-jw-crouch <153407496+me-jw-crouch@users.noreply.github.com>
2024-06-20 08:38:11 -0500

    Update EscapingHTMLEntities.php

--- PHP/EscapingHTMLEntities.php
+++ PHP/EscapingHTMLEntities.php
@@ -1,5 +1,7 @@
-// Escapes HTML entities and quotes, preventing XSS attacks.
-htmlentities($text, ENT_QUOTES, 'UTF-8');
-
-// Decodes HTML entities into their (string forms (IIRC))
-html_entity_decode($text);
+<?php
+  // Escapes HTML entities and quotes, preventing XSS attacks.
+  htmlentities($text, ENT_QUOTES, 'UTF-8');
+  
+  // Decodes HTML entities into their (string forms (IIRC))
+  html_entity_decode($text);
+?>
