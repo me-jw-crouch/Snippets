990b63208995e7bec009ce5fba409d5425f96374
me-jw-crouch <153407496+me-jw-crouch@users.noreply.github.com>
2024-06-20 08:27:13 -0500

    Create StringStuff.php

---
+++ PHP/StringStuff.php
@@ -0,0 +1,15 @@
+// Replace a word in a string.
+str_replace('Apple', 'Orange', 'My favourite fruit is an Apple.');
+
+// Replace multiple words in a string.
+str_replace(array('fruit', 'Apple'), array('Vegetable', 'Carrot'), 'My favourite fruit is an Apple.');
+
+// Check if a string contains a specific word.
+if (str_contains('My name is Me.', 'Me')) {
+  // True
+}
+
+// If older than PHP8
+if (strpos('My name is Me.', 'Me') !== false) {
+	// True
+}
